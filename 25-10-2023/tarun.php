8f1c1739208ec20b1d76fbedfdbab65d2b8743b5
spoorthi <spoorthis707@gmail.com>
2023-10-25 16:49:33 +0530

    first commit

---
+++ 25-10-2023/tarun.php
@@ -0,0 +1,8 @@
+<?php
+$a=5;
+$b=5.0;
+$type=true;
+$name="tarun";
+echo $a+$b;
+var_dump ($a);
+?> 
---
+++ 25-10-2023/varible.php
@@ -0,0 +1,12 @@
+<?php 
+$name="Hello World";
+$age=13;
+$b=4.0;
+$type=TRUE;
+echo $name;
+echo"<br>";
+echo $age;
+echo "<br>";
+echo $type;
+echo "<br>";
+?>
